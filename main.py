c0d73e63d7955339d50c5c60ce6be0edc709e546
twinsocks <twinsocks@gmail.com>
2025-11-12 16:33:12 +1100

    print report feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_char_freq
+from stats import get_num_words, get_char_freq, sort_char_freq
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,11 +7,18 @@ def get_book_text(path_to_file):
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
-    
+
     word_count = get_num_words(text)
-    print(f"Found {word_count} total words")
-    
     char_freq = get_char_freq(text)
-    print(char_freq)
+    sorted_char_freq = sort_char_freq(char_freq)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_char_freq:
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,7 +2,6 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-
 def get_char_freq(text):
     char_freq = {}
     ltext = text.lower()
@@ -13,5 +12,13 @@ def get_char_freq(text):
             char_freq[char] = 1
     return char_freq
 
+def sort_char_freq(char_freq):
+    sorted_char_freq = []
+    for char in char_freq:
+        if char.isalpha():
+            sorted_char_freq.append({"char": char, "num": char_freq[char]},)
+    def sort_on(item):
+        return item["num"]
 
-        
+    sorted_char_freq.sort(key=sort_on, reverse=True)
+    return sorted_char_freq
